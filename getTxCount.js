9ac0d752187780764b66b2d6eb95947ab989f826
Milena Monova <mimonova13@gmail.com>
2021-07-04 16:02:16 +0300

    now accepting an array of userAddresses

--- getTxCount.js
+++ getTxCount.js
@@ -1,6 +1,5 @@
 const { Validator } = require('@chainlink/external-adapter')
 const { default: axios } = require('axios')
-var eccryptoJS = require('eccrypto-js')
 
 // Define custom error scenarios for the API.
 // Return true for the adapter to retry.
@@ -14,7 +13,7 @@ const customError = (data) => {
 // with a Boolean value indicating whether or not they
 // should be required.
 const customParams = {
-  userAddress: ['userAddress'],
+  userAddresses: ['userAddresses'],
   contractAddress: ['contractAddress'],
   startBlock: ['start', 'startBlock'],
   chainID: ['chainId', 'chainID'],
@@ -26,46 +25,38 @@ const getTxCount = async (input, callback) => {
   // The Validator helps you validate the Chainlink request data
   const validator = new Validator(callback, input, customParams)
   const jobRunID = validator.validated.id
-  const userAddress = validator.validated.data.userAddress
+  const userAddresses = validator.validated.data.userAddresses;
   const contractAddress = validator.validated.data.contractAddress
   const startBlock = validator.validated.data.startBlock
   const chainID = validator.validated.data.chainID
   console.log('parameter validation passed')
 
   let finished = false
-  let totalTx = 0
   let pageNumber = 0
   let pageSize = 1000000000
-  // let pageSize = 100
-  while (!finished) {
-    const url = `https://api.covalenthq.com/v1/${chainID}/address/${userAddress}/transactions_v2/?&key=${covalentAPIKey}&no-logs=true&page-size=${pageSize}&page-number=${pageNumber}`
-    const result = await axios.get(url)
-    if (result.data.data.items.length > 0) {
-      console.log(result.data.data.items.map((z) => z.to_address))
-      console.log(
-        result.data.data.items.filter(
-          (z) =>
-            z.to_address &&
-            z.to_address == contractAddress &&
-            z.block_height > startBlock,
-        ),
-      )
-      totalTx += result.data.data.items.filter(
-        (tx) =>
-          tx.to_address &&
-          tx.block_height > startBlock &&
-          tx.to_address == contractAddress,
-      ).length
-    } else finished = true
-    pageNumber++
-    console.log('pageNum', pageNumber)
-    console.log('totalTx', totalTx)
+  let txCountPerUser = {}
+    for (var userAddress of userAddresses ) {
+    // let pageSize = 100
+    while (!finished) {
+      const url = `https://api.covalenthq.com/v1/${chainID}/address/${userAddress}/transactions_v2/?&key=${covalentAPIKey}&no-logs=true&page-size=${pageSize}&page-number=${pageNumber}`
+      const result = await axios.get(url)
+      if (result.data.data.items.length > 0) {
+        txCountPerUser[userAddress] = txCountPerUser[userAddress] ?? 0;
+        txCountPerUser[userAddress] += result.data.data.items.filter(
+          (tx) =>
+            tx.to_address &&
+            tx.block_height > startBlock &&
+            tx.to_address == contractAddress
+        ).length
+      } else finished = true
+      pageNumber++
+    }
   }
 
   const response = {
     jobRunID: jobRunID,
-    data: { totalTx },
-    result: totalTx,
+    data: { txCountPerUser },
+    result: txCountPerUser,
     statusCode: 200,
   }
 
